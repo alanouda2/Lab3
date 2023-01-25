c22d845a53b6f525b847542aeded9e50afa9c602
Alanoud <alanoud2@andrew.cmu.edu>
2023-01-25 23:24:01 +0300

    idk what we did soo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :quotes
+  get 'home' => 'home#index', as: :home
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
